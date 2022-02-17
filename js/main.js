5762b70062e3bec661cb20a16b95250abcc9c2cd
verbytska26 <alina_verbytska@outlook.com>
2022-02-17 07:41:50 +0000

    changes deal-of-month block

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(function(){
 
-$('.new-arrivals__carousel').slick({
-arrows: true,
-dots:true,
-slidesToShow: 4,
-
-
-});
-
-});
+    $('.new-arrivals__carousel').slick({
+    arrows: true,
+    dots:true,
+    slidesToShow: 4,
+    
+    
+    });
+    
+    $('.new-arrivals__carousel-slide').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt="next"></button>',
+    
+    
+    
+    })
